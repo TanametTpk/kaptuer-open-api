02a05a9a87cf1e8763d483f87de73ba20f2fe736
Tui Tpk <tanamet.tpk@gmail.com>
2020-01-04 15:14:12 +0700

    + middleware and errors

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const runServer = require('./server')
 const setup = require('./setup')
 const centroy = require('./centroy')
 
-const start = ({routes, services, port}) => {
+const start = ({routes, services, middlewares, errors, port}) => {
 
     // for new setup in future
     setup.init()
     centroy.addModule({routes, services})
+    centroy.addModule({configs:{ middlewares, errors:{handlers:errors} }})
 
     runServer(port, routes, services)
 
